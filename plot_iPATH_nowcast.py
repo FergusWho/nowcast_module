e99736c9fdd2c570b9de0c6fed4bc35ff3010ce9
junxiang <junxianghu@gmail.com>
2023-03-26 17:18:08 -0500

    bug fix in plot_iPATH_nowcast.py

--- plot_iPATH_nowcast.py
+++ plot_iPATH_nowcast.py
@@ -310,21 +310,26 @@ print (count1, count2, count3, count4)
 ######################################################################################################
 
 with open('./output.json', 'r') as read_file:
-        json_data = json.load(read_file)
-if "cme" in json_data["sep_forecast_submission"]["triggers"][0]:
-        print('triggered by CME')
-        cme_start_time = datetime.strptime(json_data["sep_forecast_submission"]["triggers"][0]["cme"]["start_time"], '%Y-%m-%dT%H:%M:%SZ')
-        time21_5 = datetime.strptime(json_data["sep_forecast_submission"]["triggers"][0]["cme"]["time_at_height"]["time"],'%Y-%m-%dT%H:%MZ')
-        simulation_zero_time = cme_start_time + (time21_5 - cme_start_time)/3.
+       json_data = json.load(read_file)
+
 
 if "flare" in json_data["sep_forecast_submission"]["triggers"][0]:
-        print('triggered by flare')
-        flare_start_time = datetime.strptime(json_data["sep_forecast_submission"]["triggers"][0]["flare"]["start_time"], '%Y-%m-%dT%H:%MZ')
-        FSXR=json_data["sep_forecast_submission"]["triggers"][0]["flare"]["intensity"]
-        Vcme = 2.4e4*FSXR**0.3 # km/s
-        time_to_inner = 0.05*AU/(Vcme*1000.)/3600.*2/3.
-        print(time_to_inner)
-        simulation_zero_time = flare_start_time + timedelta(hours=time_to_inner)
+       print('triggered by flare')
+       flare_start_time = datetime.strptime(json_data["sep_forecast_submission"]["triggers"][0]["flare"]["start_time"], '%Y-%m-%dT%H:%MZ')
+       FSXR=json_data["sep_forecast_submission"]["triggers"][0]["flare"]["intensity"]
+       Vcme = 2.4e4*FSXR**0.3 # km/s
+       time_to_inner = 0.05*AU/(Vcme*1000.)/3600.*2/3.
+       print(time_to_inner)
+       simulation_zero_time = flare_start_time + timedelta(hours=time_to_inner)
+else:
+       if "cme" in json_data["sep_forecast_submission"]["triggers"][0]:
+              print('triggered by CME')
+              cme_start_time = datetime.strptime(json_data["sep_forecast_submission"]["triggers"][0]["cme"]["start_time"], '%Y-%m-%dT%H:%M:%SZ')
+              time21_5 = datetime.strptime(json_data["sep_forecast_submission"]["triggers"][0]["cme"]["time_at_height"]["time"],'%Y-%m-%dT%H:%MZ')
+              simulation_zero_time = cme_start_time + (time21_5 - cme_start_time)/3.
+       else:
+              print('ERROR - No trigger info in output.json')
+
 
 simulation_end_time = simulation_zero_time + timedelta(hours=xtime[t_num-1])
 start_time = simulation_zero_time.strftime('%Y-%m-%dT%H:%M:%SZ')
