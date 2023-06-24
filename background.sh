bddfa8aeda09a9cec1f4dc7354b3e5791d675736
iPATH User <m_ipath@ip-172-30-54-224.us-west-2.compute.internal>
2023-06-24 16:32:17 +0000

    Improved logging and cleanup

--- background.sh
+++ background.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-iPATH_dir='/shared/iPATH/ipath_v2'
-root_dir='/shared/iPATH/nowcast_module_v1'
-data_dir='/data/iPATH/nowcast_module_v1'
-# default for CCMC AWS on rt-hpc-prod
 
+# default for CCMC AWS on rt-hpc-prod
+iPATH_dir='/shared/iPATH/ipath_v2'
+code_dir='/shared/iPATH/nowcast_module_v1'
+data_dir='/data/iPATH/nowcast_module_v1'
+
+echo "----------- Background Module -----------"
+
+# default values for command-line arguments
 run_time=$(date +'%Y%m%d_%H%M' -u)
 if_local=0
 
@@ -16,54 +20,91 @@ do
         L) if_local=1;;
     esac
 done
+echo "[$(date -u +'%F %T')] Run time: $run_time"
 
 if [ $if_local -eq 1 ]
 then
     # change these accordingly if you want to run locally
     iPATH_dir=$HOME'/iPATH2.0'
-    root_dir=$HOME'/nowcast_module'
+    code_dir=$HOME'/nowcast_module'
 else
     source ~/setup_pkgs
 fi
+echo "-----------------------------------------"
+echo
 
+# create output folder
 mkdir -p $data_dir
 
-cd $root_dir
+# download solar wind quantities from iSWA
+# write the run folder name in temp.txt
+# create the file ${run_dir}_input.json with simulation input parameters
+echo "[$(date -u +'%F %T')] Downloading solar wind data ..."
+python3 $code_dir/grepSW.py --root_dir $data_dir --run_time $run_time
+echo "[$(date -u +'%F %T')] Done"
+echo
 
-python3 $root_dir/grepSW.py --root_dir $data_dir --run_time $run_time
-
-run_dir=`cat $data_dir/temp.txt`
+run_dir=$(cat $data_dir/temp.txt)
 rm $data_dir/temp.txt
 
+echo "[$(date -u +'%F %T')] Background simulation: $run_dir"
 
-mkdir -p $data_dir/Background/$run_dir
-cp -r $iPATH_dir/Acceleration/zeus3.6/* $data_dir/Background/$run_dir
+bkg_dir=$data_dir/Background/$run_dir
+logfile=$bkg_dir/log.txt
+
+echo "[$(date -u +'%F %T')] Copying files to $bkg_dir ..."
+mkdir $bkg_dir
+# copy ZEUS source code
+cp -r $iPATH_dir/Acceleration/zeus3.6/* $bkg_dir/
+
 # use the modified dzeus36 version for nowcasting
-cp $root_dir/dzeus36_alt $data_dir/Background/$run_dir/dzeus36
+cp $code_dir/dzeus36_alt $bkg_dir/dzeus36
 
-mv $data_dir/${run_dir}_input.json $data_dir/Background/$run_dir/input.json
+mv $data_dir/${run_dir}_input.json $bkg_dir/input.json
+echo "[$(date -u +'%F %T')] Done"
+echo
+echo "[$(date -u +'%F %T')] Switching to $logfile"
 
+cd $bkg_dir
 
-python3 $iPATH_dir/prepare_PATH.py --root_dir $data_dir/Background/$run_dir --path_dir $iPATH_dir --run_mode 1 --input $data_dir/Background/$run_dir/input.json
+# modify ZEUS source code according to the input json file
+echo "[$(date -u +'%F %T')] Setting up background module ..." >>$logfile
+python3 $iPATH_dir/prepare_PATH.py --root_dir $bkg_dir --path_dir $iPATH_dir --run_mode 1 --input $bkg_dir/input.json >>$logfile 2>&1
+echo "[$(date -u +'%F %T')] Done" >>$logfile
+echo >>$logfile
 
-cd $data_dir/Background/$run_dir
-csh -v ./iPATH_zeus.s
+echo "[$(date -u +'%F %T')] Compiling ZEUS ..." >>$logfile
+csh -v ./iPATH_zeus.s >>$logfile 2>&1
+echo "[$(date -u +'%F %T')] Done" >>$logfile
+echo >>$logfile
 
+echo "[$(date -u +'%F %T')] Running background module ..." >>$logfile
 if [ $if_local -eq 1 ]
 then
-    ./xdzeus36
-    cd ..
+    ./xdzeus36 >>$logfile 2>&1
 else
-    cd $data_dir/Background/$run_dir
-    sbatch -W $root_dir/run_zeus.sh -r $data_dir/Background/$run_dir
-fi
+    sbatch -W $code_dir/run_zeus.sh -r $bkg_dir >>$logfile 2>&1
 
-#clean up some files
-rm $data_dir/Background/$run_dir/zr001JH
-rm $data_dir/Background/$run_dir/zr002JH
-rm $data_dir/Background/$run_dir/zr003JH
-rm $data_dir/Background/$run_dir/zr004JH
-rm $data_dir/Background/$run_dir/zhto00*JH
-rm $data_dir/Background/$run_dir/zhto01*JH
-rm $data_dir/Background/$run_dir/zhto02*JH
-rm -r $data_dir/Background/$run_dir/releases
+    # compress Slurm logfile
+    for f in slurm*.out; do
+        gzip $f
+    done
+fi
+echo "[$(date -u +'%F %T')] Done" >>$logfile
+echo >>$logfile
+
+echo "[$(date -u +'%F %T')] Cleaning up ..." >>$logfile
+# ZEUS restart files before steady-state solution
+rm zr001JH
+rm zr002JH
+rm zr003JH
+rm zr004JH
+
+# ZEUS dump files before steady-state solution
+rm zhto00*JH
+rm zhto01*JH
+rm zhto02*JH
+
+# unused ZEUS source files
+rm -r releases
+echo "[$(date -u +'%F %T')] Done" >>$logfile
--- flare.sh
+++ flare.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+
+# default values for CCMC AWS on rt-hpc-prod
 iPATH_dir='/shared/iPATH/ipath_v2'
-root_dir='/shared/iPATH/nowcast_module_v1'
+code_dir='/shared/iPATH/nowcast_module_v1'
 data_dir='/data/iPATH/nowcast_module_v1'
 opsep_dir='/shared/iPATH/operational_sep_v3'
-# default for CCMC AWS on rt-hpc-prod
 
 MPI_comp='mpif90'
 FCOMP='gfortran'
 
+echo "------------- Flare Module -------------"
+
+# default values for command-line arguments
 run_time=$(date +'%Y%m%d_%H%M' -u)
 if_local=0
 
-echo "-----------------------------------------"
-echo $run_time
-
 # testing for specific event:
-# example: bash CME.sh -t '20220120_0830'
+# example: bash CME.sh -t 20220120_0830
 while getopts 't:L' flag
 do
     case "${flag}" in
@@ -23,30 +24,37 @@ do
         L) if_local=1;;
     esac
 done
+echo "[$(date -u +'%F %T')] Run time: $run_time"
 
 if [ $if_local -eq 1 ]
 then
     # change these accordingly if you want to run locally
     iPATH_dir=$HOME'/iPATH2.0'
-    root_dir=$HOME'/nowcast_module'
+    code_dir=$HOME'/nowcast_module'
     thread_count=12
 else
     source ~/setup_pkgs
     thread_count=64
 fi
+echo "-----------------------------------------"
+echo
 
+# create/copy necessary output folders
 mkdir -p $data_dir/Flare
-[[ ! -d $data_dir/helioweb ]] && cp -r $root_dir/helioweb $data_dir/
+[[ ! -d $data_dir/helioweb ]] && cp -r $code_dir/helioweb $data_dir/
 
-CME_dir=$run_time
-
-trspt_dir='transport'
-
-# create already processed flare list, if not existent
+# create processed flare list, if not existent
 [[ ! -f $data_dir/pastflare.json ]] && echo "[]" >$data_dir/pastflare.json
 
-# read last line of output from check_CME.py
-last_line=`python3 $root_dir/check_flare.py --root_dir $data_dir --run_time $run_time | tail -n 1`
+# look for new flares from DONKI
+# create the input parameters files for Earth: $bgsw_folder_name/${run_time}_flare_input.json
+# last line is: bgsw_folder_name flare_id
+# read last line of output from check_flare.py
+echo "[$(date -u +'%F %T')] Checking for new flares ..."
+last_line=$(python3 $code_dir/check_flare.py --root_dir $data_dir --run_time $run_time | tail -n 1)
+echo "[$(date -u +'%F %T')] Done"
+echo
+
 IFS=' '
 read -a strarr <<<$last_line
 bgsw_folder_name=${strarr[0]}
@@ -54,123 +62,171 @@ CME_id=${strarr[1]}     # this is actually flare_id
 
 # get the start and end date for Opsep
 startdate=(${bgsw_folder_name//_/ })
+startdate_opsep=${startdate:0:4}-${startdate:4:2}-${startdate:6:2}
 enddate=$(date -d "$startdate + 2 days" +'%Y-%m-%d')
 
-echo $bgsw_folder_name
+echo "[$(date -u +'%F %T')] Background simulation: $bgsw_folder_name"
 
 if [ -z "$bgsw_folder_name" ]
 then
-    echo "There is no flare"
-
+    echo "[$(date -u +'%F %T')] There is no flare: exit"
 else
     # abort if no background simulation exists
     [[ ! -d $data_dir/Background/$bgsw_folder_name ]] && {
-      echo "Background simulation $bgsw_folder_name not found"
+      echo "[$(date -u +'%F %T')] Background simulation not found: exit"
       exit 1
     }
 
     #-----------------------------------------------
     # CME setup and acceleration:
-    cp -r $data_dir/Background/$bgsw_folder_name $data_dir/Flare/$CME_id
-    echo "CME found! Checking Time: "$run_time >$data_dir/Flare/${CME_id}/log.txt
-    echo "CME id: "$CME_id >>$data_dir/Flare/$CME_id/log.txt
-    echo "current time: "$(date +'%Y-%m-%d_%H:%M' -u) >>$data_dir/Flare/$CME_id/log.txt
+    CME_dir=$data_dir/Flare/$CME_id
+    logfile=$CME_dir/log.txt
 
-    # delete residual files from other simulations
-    rm $data_dir/Flare/$CME_id/slurm*.out # slurm log from Background simulation
-    find $data_dir/Flare/$CME_id -type f -name '*.json' | grep -v ${CME_dir}_flare | xargs rm # json files from other CME/Flare simulations
+    echo "[$(date -u +'%F %T')] Copying background simulation to $CME_dir ..."
+    cp -r $data_dir/Background/$bgsw_folder_name $CME_dir
 
     # use the modified dzeus36 version for nowcasting
-    cp $root_dir/dzeus36_alt $data_dir/Flare/$CME_id/dzeus36
+    cp $code_dir/dzeus36_alt $CME_dir/dzeus36
+    echo "[$(date -u +'%F %T')] Done"
+    echo
+    echo "[$(date -u +'%F %T')] Switching to $logfile"
 
-    python3 $iPATH_dir/prepare_PATH.py --root_dir $data_dir/Flare/$CME_id --path_dir $iPATH_dir --run_mode 0 --input $data_dir/Flare/$CME_id/${CME_dir}_flare_input.json >>$data_dir/Flare/$CME_id/log.txt 2>&1
-    
-    cp $data_dir/Flare/$CME_id/${CME_dir}_flare_input.json $data_dir/Flare/$CME_id/CME_input.json
+    cd $CME_dir
 
-    cd $data_dir/Flare/$CME_id
-    csh -v ./iPATH_zeus.s
+    echo "[$(date -u +'%F %T')] Flare found! Checking Time: $run_time" >>$logfile
+    echo "[$(date -u +'%F %T')] Flare id: $CME_id" >>$logfile
+
+    # delete residual files created by other CME/Flare simulations in the copied Background folder
+    echo "[$(date -u +'%F %T')] Deleting residual files ..." >>$logfile
+    rm -f slurm* # Slurm logfiles from Background simulation
+    find -type f -name '*.json' | grep -v ${run_time}_flare | xargs rm -f # json files from other CME/Flare simulations
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+
+    # modify ZEUS source code according to the input json file
+    echo "[$(date -u +'%F %T')] Setting up acceleration module ..." >>$logfile
+    python3 $iPATH_dir/prepare_PATH.py --root_dir $CME_dir --path_dir $iPATH_dir --run_mode 0 --input ${run_time}_flare_input.json >>$logfile 2>&1
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+
+    # CME_input.json used by plot_CME_info.py
+    cp ${run_time}_flare_input.json CME_input.json
+
+    echo "[$(date -u +'%F %T')] Compiling ZEUS ..." >>$logfile
+    csh -v ./iPATH_zeus.s >>$logfile 2>&1
+    echo "[$(date -u +'%F %T')] Compilation done" >>$logfile
+    echo >>$logfile
+
+    echo "[$(date -u +'%F %T')] Running acceleration module" >>$logfile
     if [ $if_local -eq 1 ]
     then
-        ./xdzeus36 <input
+        ./xdzeus36 <input >>$logfile 2>&1
     else
-        cd $data_dir/Flare/$CME_id
-        sbatch -W $root_dir/run_zeus2.sh -r $data_dir/Flare/$CME_id
-    fi
+        sbatch -W $code_dir/run_zeus2.sh -r $CME_dir >>$logfile 2>&1
 
-    echo "CME setup and acceleration done. Time: "$(date +'%Y-%m-%d_%H:%M' -u) >>$data_dir/Flare/$CME_id/log.txt 
-    cd $root_dir
-    
+        # compress Slurm logfile
+        for f in slurm*.out; do
+           gzip $f >>$logfile 2>&1
+        done
+    fi
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+
     #-----------------------------------------------------------------------------------------
     # setup and compile for the transport module
-    
-    python3 $iPATH_dir/prepare_PATH.py --root_dir $data_dir/Flare/$CME_id --path_dir $iPATH_dir --run_mode 2 --ranks $thread_count --input $data_dir/Flare/$CME_id/${CME_dir}_flare_input.json >>$data_dir/Flare/$CME_id/log.txt 2>&1
-    
-    mkdir $data_dir/Flare/$CME_id/path_output/$trspt_dir
-    
-    $MPI_comp -O3 $iPATH_dir/Transport/parallel_wrapper.f $iPATH_dir/Transport/transport_2.05.f -o $data_dir/Flare/$CME_id/path_output/$trspt_dir/trspt.out
-    $FCOMP $iPATH_dir/Transport/combine.f -o $data_dir/Flare/$CME_id/path_output/$trspt_dir/combine.out
 
-    cp $root_dir/plot_CME_info.py $data_dir/Flare/$CME_id/path_output
-    cp $iPATH_dir/Transport/trspt_input $data_dir/Flare/$CME_id/path_output/$trspt_dir
-    cp $root_dir/plot_iPATH_nowcast.py $data_dir/Flare/$CME_id/path_output/$trspt_dir
-    cp $data_dir/Flare/$CME_id/${CME_dir}_flare_input.json $data_dir/Flare/$CME_id/path_output/$trspt_dir
-    mv $data_dir/Flare/$CME_id/${CME_dir}_flare_output.json $data_dir/Flare/$CME_id/path_output/$trspt_dir/output.json
-   
+    # modify iPATH source code according to the input json file
+    echo "[$(date -u +'%F %T')] Setting up transport module for Earth ..." >>$logfile
+    python3 $iPATH_dir/prepare_PATH.py --root_dir $CME_dir --path_dir $iPATH_dir --run_mode 2 --ranks $thread_count --input ${run_time}_flare_input.json >>$logfile 2>&1
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+
+    trspt_dir=$CME_dir/path_output/transport
+    mkdir $trspt_dir
+    cd $trspt_dir
+
+    echo "[$(date -u +'%F %T')] Compiling iPATH ..." >>$logfile
+    $MPI_comp -O3 $iPATH_dir/Transport/parallel_wrapper.f $iPATH_dir/Transport/transport_2.05.f -o trspt.out >>$logfile 2>&1
+    $FCOMP $iPATH_dir/Transport/combine.f -o combine.out >>$logfile 2>&1
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+
+    cd $CME_dir
+
+    echo "[$(date -u +'%F %T')] Copying files to $trspt_dir ..." >>$logfile
+    cp $iPATH_dir/Transport/trspt_input $trspt_dir
+    cp ${run_time}_flare_input.json $trspt_dir
+    mv ${run_time}_flare_output.json $trspt_dir/output.json
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+
+    #-----------------------------------------------------------------------------------------
+    echo "[$(date -u +'%F %T')] Running transport module for Earth ..." >>$logfile
+    cd $trspt_dir
     if [ $if_local -eq 1 ]
     then
-        cd $data_dir/Flare/$CME_id/path_output/$trspt_dir
-        mpirun -np $thread_count trspt.out
+        mpirun -np $thread_count trspt.out >>$logfile 2>&1
     else
-        cd $data_dir/Flare/$CME_id
-        sbatch -W $root_dir/run_transport.sh -r $data_dir/Flare/$CME_id/path_output/$trspt_dir    
+        sbatch -W $code_dir/run_transport.sh -r $trspt_dir >>$logfile 2>&1
+
+        # compress Slurm logfile
+        for f in slurm*.out; do
+           gzip $f >>$logfile 2>&1
+        done
     fi
+    ./combine.out >>$logfile 2>&1
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
 
-    echo "Transport for Earth done. Time: "$(date +'%Y-%m-%d_%H:%M' -u) >>$data_dir/Flare/$CME_id/log.txt
-
-    cd $data_dir/Flare/$CME_id/path_output/$trspt_dir
-    ./combine.out
-    # clean up some unused output
-    rm RawData*
-    rm $data_dir/Flare/$CME_id/path_output/dist_all_shl.dat
-    
-    # Plot result:
-    python3 $data_dir/Flare/$CME_id/path_output/$trspt_dir/plot_iPATH_nowcast.py
-    cd $data_dir/Flare/$CME_id/path_output
-    python3 $data_dir/Flare/$CME_id/path_output/plot_CME_info.py
-
-    # compress transport files
-    tar --remove-files -zcf $data_dir/Flare/$CME_id/path_output/$trspt_dir/fp.tar.gz $data_dir/Flare/$CME_id/path_output/$trspt_dir/fp_*
-
-    convert -delay 5 $data_dir/Flare/$CME_id/path_output/CME*.png $data_dir/Flare/$CME_id/path_output/CME.gif
-
-    # remove source pngs if the animation exists and contains the right number of frames
-    if [[ -f $data_dir/Flare/$CME_id/path_output/CME.gif ]]; then
-      npngs=$(ls $data_dir/Flare/$CME_id/path_output/CME*.png | wc -l)
-      nframes=$(identify $data_dir/Flare/$CME_id/path_output/CME.gif | wc -l)
-      (( npngs == nframes )) && rm $data_dir/Flare/$CME_id/path_output/CME*.png
-    fi
-
-    # compress intermediate acceleration files
-    for f in $data_dir/Flare/$CME_id/path_output/{observer_pov.dat,kappa-par-perp.dat,all_shell_bndy.dat,dist_at_shock.dat,esc_distr*,momenta-hi.dat,solar_wind_profile.dat}; do
-      gzip $f
-    done
+    echo "[$(date -u +'%F %T')] Plotting transport results ..." >>$logfile
+    python3 $code_dir/plot_iPATH_nowcast.py >>$logfile 2>&1
+    echo "[$(date -u +'%F %T')]  Done" >>$logfile
+    echo >>$logfile
 
     # Use OpSep to produce output for SEP scoreboard
-    echo "Now using OpSEP to generate output:" >>$data_dir/Flare/$CME_id/log.txt
-    cd $trspt_dir
+    echo "[$(date -u +'%F %T')] Using OpSEP to generate output ..." >>$logfile
     mkdir -p json/{library,data,output}
     cp output.json json/library/model_template.json
     cp ${startdate}_differential_flux.csv json/data/
     cd json
-    python3 $opsep_dir/operational_sep_quantities.py --StartDate ${startdate:0:4}-${startdate:4:2}-${startdate:6:2} --EndDate $enddate --Experiment user --ModelName ZEUS+iPATH_flare --FluxType differential --UserFile ${startdate}_differential_flux.csv --spase spase://CCMC/SimulationModel/iPATH/2 >>$data_dir/Flare/$CME_id/log.txt 2>&1
-    find -type d -empty -delete # remove empty folders created by opsep
+    python3 $opsep_dir/operational_sep_quantities.py --StartDate $startdate_opsep --EndDate $enddate --Experiment user --ModelName ZEUS+iPATH_Flare --FluxType differential --UserFile ${startdate}_differential_flux.csv --spase spase://CCMC/SimulationModel/iPATH/2 >>$logfile 2>&1
 
-    cd $root_dir
+    # remove empty folders created by opsep
+    find -type d -empty -delete
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+    #-----------------------------------------------------------------------------------------
 
-    # compress Slurm logs
-    for f in $data_dir/Flare/$CME_id/slurm*.out; do
-      gzip $f
+    echo "[$(date -u +'%F %T')] Making CME movie ..." >>$logfile
+    cd $CME_dir/path_output
+    python3 $code_dir/plot_CME_info.py >>$logfile 2>&1
+    convert -delay 5 CME*.png CME.gif >>$logfile 2>&1
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+
+    echo "[$(date -u +'%F %T')] Cleaning up ..." >>$logfile
+    # remove source pngs if the animation exists and contains the right number of frames
+    if [[ -f CME.gif ]]; then
+      npngs=$(ls CME*.png | wc -l)
+      nframes=$(identify CME.gif | wc -l)
+      (( npngs == nframes )) && rm CME*.png
+    fi
+
+    # compress intermediate acceleration files
+    for f in observer_pov.dat kappa-par-perp.dat all_shell_bndy.dat dist_at_shock.dat esc_distr* momenta-hi.dat solar_wind_profile.dat; do
+      gzip $f >>$logfile 2>&1
     done
 
-fi
+    # clean up unneeded intermediate acceleration files
+    rm dist_all_shl.dat
 
+    cd $trspt_dir
+
+    # clean up some unused output
+    rm RawData*
+
+    # compress transport files
+    tar --remove-files -zcf fp.tar.gz fp_* >>$logfile 2>&1
+    echo "[$(date -u +'%F %T')] Done" >>$logfile
+    echo >>$logfile
+fi
