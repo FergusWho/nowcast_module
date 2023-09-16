4554d51fa7e6c862e3dcd6c68076865abaa268ba
iPATH User <m_ipath@ip-172-30-54-224.us-west-2.compute.internal>
2023-09-16 19:29:19 +0000

    Use full path, so it works when called from cron

--- DailyReport.sh
+++ DailyReport.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cwd=$(dirname "$(realpath "$0")")
+
 # redirect output to both stdout and mailx
-./FindSimulationProblems.sh -f yesterday -t today -p no \
+$cwd/FindSimulationProblems.sh -f yesterday -t today -p no \
 |& tee >(mailx -s "iPATH rt-hpc-prod summary" -r m_ipath corti@hawaii.edu)
