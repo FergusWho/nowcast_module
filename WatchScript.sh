7f00ef3a2e7cd038e8d17d8ecc1a8cad68fc3fac
iPATH User <m_ipath@ip-172-30-54-224.us-west-2.compute.internal>
2023-10-27 10:00:46 +0000

    Sort more recent runs on top

--- WatchScript.sh
+++ WatchScript.sh
@@ -8,7 +8,7 @@ n=$(( w/32 ))
 
 echo "  --- Simulation jobs ---"
 squeue --me -h -o "%.18i %.10M %.6D %R %Z" \
-| sed -E "s/^ +/  /"
+| column -t
 echo
 
 echo "  --- Simulation logs (most recent 10/type) ---"
@@ -18,7 +18,7 @@ find -mindepth 3 -type f -name log.txt -printf "%P\n" \
    $0 ~ /Background/ && nbkg < 10 { ++nbkg; print $0 }
    $0 ~ /CME/ && ncme < 10 { ++ncme; print $0 }
    $0 ~ /Flare/ && nflr < 10 { ++nflr; print $0 }' \
-| sort -V \
+| sort -rV \
 | xargs wc -l \
 | sed -E -e'$d' -e"s|[^ /]+/||" -e"s|/log.txt||" \
 | pr -${n}T -W$w
@@ -31,7 +31,7 @@ find cron -type f \
    $0 ~ /Background/ && nbkg < 10 { ++nbkg; print $0 }
    $0 ~ /CME/ && ncme < 10 { ++ncme; print $0 }
    $0 ~ /Flare/ && nflr < 10 { ++nflr; print $0 }' \
-| sort -V \
+| sort -rV \
 | xargs wc -l \
 | sed -E -e'$d' -e"s|cron/||" -e"s/.log//" \
 | pr -${n}T -W$w
@@ -46,7 +46,7 @@ find staging/iswa -type f -printf '%P\n' \
 | awk '
    $0 ~ /iPATH_CME/ && ncme < 10 { ++ncme; print $0 }
    $0 ~ /iPATH_Flare/ && nflr < 10 { ++nflr; print $0 }' \
-| sort -k2V \
+| sort -k2rV \
 | sed -E -e's/ZEUS\+iPATH_//' \
 | pr -${n}T -W$w
 echo
@@ -60,6 +60,6 @@ find staging/sep_scoreboard -type f -printf '%P\n' \
 | awk '
    $0 ~ /iPATH_CME/ && ncme < 10 { ++ncme; print $0 }
    $0 ~ /iPATH_Flare/ && nflr < 10 { ++nflr; print $0 }' \
-| sort -k2V \
+| sort -k2rV \
 | sed -E -e's/ZEUS\+iPATH_//' -e's/_differential//' \
 | pr -${n}T -W$w
