6685961d396ea223c72b5984b87aff07d8980595
iPATH User <m_ipath@ip-172-30-54-224.us-west-2.compute.internal>
2024-02-06 17:13:55 +0000

    Improved error handling and minimized differences between the two scripts

--- check_CME.py
+++ check_CME.py
@@ -68,9 +68,7 @@ startdate = (utc_datetime - timedelta(days=7) ).strftime("%Y-%m-%d")
 
 #------ get the list for target date -----
 url_cme = "https://kauai.ccmc.gsfc.nasa.gov/DONKI/WS/get/CMEAnalysis?startDate="+startdate+"&endDate="+enddate+"&mostAccurateOnly=true&speed=450&halfAngle=15"
-print('DONKI URL:', file=sys.stderr)
-print(url_cme, file=sys.stderr)
-
+print('DONKI URL:', url_cme, file=sys.stderr)
 
 # constants to avoid infinite loops during DONKI requests
 MAX_REQUESTS = 100
@@ -115,24 +113,27 @@ with open(root_dir+'/CME/past.json') as cme_list:
 
 for i in range(0, len(data)):
    try:
-      cme_start_time = data[i].get('associatedCMEID').split("-CME-")[0]
+      # expected ID format: yyyy-mm-ddTHH:MM:SS-CME-xxx
+      CME_id = data[i].get('associatedCMEID')
+      CME_start_time = CME_id.split("-CME-")[0]
 
       # expected URL format for CME analyses:
       #    https://kauai.ccmc.gsfc.nasa.gov/DONKI/view/CMEAnalysis/xxxxx/-1
-      cme_analysis_id = data[i].get('link').split('/')[6]
+      CME_link = data[i].get('link')
+      CME_analysis_id = CME_link.split('/')[6]
 
-      datetime_CME = datetime.strptime(cme_start_time, '%Y-%m-%dT%H:%M:%S')
-      print('[{:2d}] CME date: {}, analysis: {}'.format(i, datetime_CME, cme_analysis_id), file=sys.stderr)
+      datetime_CME = datetime.strptime(CME_start_time, '%Y-%m-%dT%H:%M:%S')
+      print('[{:2d}] CME date: {}, analysis: {}'.format(i, datetime_CME, CME_analysis_id), file=sys.stderr)
 
       if datetime_CME > dt_start and datetime_CME <= utc_datetime:
          # check whether this CME has been simulated before
          # use either associatedCMEID or (associatedCMEID + link) if link is available in past.json
-         result = [x for x in list_obj if x.get('associatedCMEID') == data[i].get('associatedCMEID') and (x.get('link') is None or x.get('link') == data[i].get('link')) ]
+         result = [x for x in list_obj if x.get('associatedCMEID') == CME_id and (x.get('link') is None or x.get('link') == CME_link) ]
 
          if result == []:
             # no run found for this CME, new CME detected:
             CME_index.append(i)
-            print('     New CME found:', data[i].get('associatedCMEID'), data[i].get('link'), file=sys.stderr)
+            print('     New CME found:', CME_id, CME_link, file=sys.stderr)
          else:
             print('     Previous simulation run found:', result, file=sys.stderr)
 
@@ -151,29 +152,44 @@ f4 = open(root_dir+'/CME/log.txt', 'a')
 print('Last CME_index:', ii, file=sys.stderr)
 
 if len(CME_index) != 0:
+    try:
+       CME_id        = data[CME_index[ii]].get('associatedCMEID')
+       CME_link      = data[CME_index[ii]].get('link')
+       CME_speed     = data[CME_index[ii]].get('speed')
+       CME_halfAngle = data[CME_index[ii]].get('halfAngle')
+       CME_latitude  = data[CME_index[ii]].get('latitude')
+       CME_longitude = data[CME_index[ii]].get('longitude')
+       CME_time21_5  = data[CME_index[ii]].get('time21_5')
+
+       CME_start_time = CME_id.split("-CME-")[0]
+       datetime_CME = datetime.strptime(CME_start_time, '%Y-%m-%dT%H:%M:%S')
+
+       # expected URL format for CME analyses:
+       #    https://kauai.ccmc.gsfc.nasa.gov/DONKI/view/CMEAnalysis/xxxxx/-1
+       CME_analysis_id = CME_link.split('/')[6]
+
+       target_date = datetime_CME.strftime('%Y-%m-%d')
+       t1 = datetime.strptime(target_date, '%Y-%m-%d')     # 00:00
+       t2 = t1 + timedelta(hours=8)                        # 08:00
+       t3 = t2 + timedelta(hours=8)                        # 16:00
+
+       phi_e = 100.0 - CME_longitude
+
+    except Exception as e:
+       traceback.print_exception(e)
+       print('[{:2d}] JSON data:'.format(i), file=sys.stderr)
+       json.dump(data[CME_index[ii]], sys.stderr, indent=3)
+
     list_obj.append({
-      "associatedCMEID": data[CME_index[ii]].get('associatedCMEID'),
-      "link": data[CME_index[ii]].get('link')
+      "associatedCMEID": CME_id,
+      "link": CME_link
     })
     with open(root_dir+'/CME/past.json', 'w') as write_file:
        json.dump(list_obj, write_file, indent=4)
 
-    cme_id = data[CME_index[ii]].get('associatedCMEID')
-    cme_start_time = cme_id.split("-CME-")[0]
-    datetime_CME = datetime.strptime(cme_start_time, '%Y-%m-%dT%H:%M:%S')
-
-    # expected URL format for CME analyses:
-    #    https://kauai.ccmc.gsfc.nasa.gov/DONKI/view/CMEAnalysis/xxxxx/-1
-    cme_analysis_id = data[CME_index[ii]].get('link').split('/')[6]
-
-    target_date = datetime_CME.strftime('%Y-%m-%d')
-    t1 = datetime.strptime(target_date, '%Y-%m-%d')     # 00:00
-    t2 = t1 + timedelta(hours=8)                        # 08:00
-    t3 = t2 + timedelta(hours=8)                        # 16:00
-
-    f4.write('Checking Time:{} | CME found:{}_{} speed:{} width:{} lat:{} lon:{}\n'.format(
-      utc_time, cme_id, cme_analysis_id, data[CME_index[ii]].get('speed'),
-      data[CME_index[ii]].get('halfAngle')*2, data[CME_index[ii]].get('latitude'), data[CME_index[ii]].get('longitude')))
+    f4.write('Checking Time:{} | CME found:{}_{} speed:{} width:{} lat:{} lon:{} time_21.5:{}\n'.format(
+      utc_time, CME_id, CME_analysis_id, CME_speed,
+      CME_halfAngle*2, CME_latitude, CME_longitude, CME_time21_5))
 
     # assuming the background solar wind setup can finish in 15 mins
     if datetime_CME < t2:
@@ -193,25 +209,22 @@ if len(CME_index) != 0:
        exit_after_error(utc_time, 'Missing background folder', 'ERROR:MISSING_BKG')
 
     #### modify input.json for the CME run
-    f2 = open(root_dir+'/Background/'+bgsw_folder_name+'/input.json', 'r')
-    input_data = json.load(f2)
-    f2.close()
+    with open(root_dir+'/Background/'+bgsw_folder_name+'/input.json', 'r') as f2:
+       input_data = json.load(f2)
 
-    input_data['cme_speed'] = data[CME_index[ii]].get('speed') * np.cos(data[CME_index[ii]].get('latitude')*pi/180.0)
-    input_data['cme_width'] = data[CME_index[ii]].get('halfAngle')*2
-    input_data['phi_e'] = 100.0-data[CME_index[ii]].get('longitude')
+    input_data['cme_speed'] = CME_speed * np.cos(CME_latitude*pi/180.0)
+    input_data['cme_width'] = CME_halfAngle*2
+    input_data['phi_e'] = phi_e
 
     # change density multiplier based on CME speed
-    if data[CME_index[ii]].get('speed') >= 1500:
+    if CME_speed >= 1500:
         n_multi = 4.0
     else:
-        n_multi = data[CME_index[ii]].get('speed')*2e-3 + 1.
-
+        n_multi = CME_speed*2e-3 + 1.
     input_data['n_multi'] = n_multi
 
-    f3 = open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_CME_earth_input.json', 'w')
-    json.dump(input_data, f3, indent=4)
-    f3.close()
+    with open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_CME_earth_input.json', 'w') as f3:
+       json.dump(input_data, f3, indent=4)
 
     earth_r, earth_lat, earth_lon = find_location('planets/earth', datetime_CME, root_dir)
 
@@ -222,12 +235,11 @@ if len(CME_index) != 0:
 
        rad, lat, lon = find_location(loc, datetime_CME, root_dir)
 
-       input_data['phi_e'] = 100.0-data[CME_index[ii]].get('longitude') + lon - earth_lon
+       input_data['phi_e'] = phi_e + lon - earth_lon
        input_data['r0_e'] = rad
 
-       f3 = open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_CME_'+obs+'_input.json', 'w')
-       json.dump(input_data, f3, indent=4)
-       f3.close()
+       with open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_CME_'+obs+'_input.json', 'w') as f3:
+          json.dump(input_data, f3, indent=4)
 
     #### Generating Output JSON
     json_data={"sep_forecast_submission":{
@@ -276,20 +288,20 @@ if len(CME_index) != 0:
         ]
     }}
 
-    cme_start_time_fixed = cme_start_time+"Z"
+    cme_start_time_fixed = CME_start_time+"Z"
     cme = {
            "cme":{
            "start_time":cme_start_time_fixed,
-           "lat": data[CME_index[ii]].get('latitude'),
-           "lon": data[CME_index[ii]].get('longitude'),
-           "half_width": data[CME_index[ii]].get('halfAngle'),
-           "speed": data[CME_index[ii]].get('speed'),
+           "half_width": CME_halfAngle,
+           "speed": CME_speed,
+           "lat": CME_latitude,
+           "lon": CME_longitude,
            "height": 21.5,
-           "time_at_height": { "time":data[CME_index[ii]].get('time21_5'), "height": 21.5 },
+           "time_at_height": { "time": CME_time21_5, "height": 21.5 },
            "coordinates": "HEEQ",
            "catalog": "DONKI",
-           "catalog_id": data[CME_index[ii]].get('associatedCMEID'),
-           "urls": [ data[CME_index[ii]].get('link') ]
+           "catalog_id": CME_id,
+           "urls": [ CME_link ]
            }
     }
 
@@ -297,7 +309,7 @@ if len(CME_index) != 0:
 
     with open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_CME_earth_output.json', 'w') as write_file:
         json.dump(json_data, write_file, indent=4)
-    CME_id = cme_id.replace('-', '', 2).replace(':', '', 2) + '_' + cme_analysis_id
+    CME_id = CME_id.replace('-', '', 2).replace(':', '', 2) + '_' + CME_analysis_id
 else:
     bgsw_folder_name=''
     CME_id = ''
--- check_flare.py
+++ check_flare.py
@@ -46,11 +46,11 @@ if (run_time == ""):
        utc_datetime = now.astimezone(utc)
        utc_datetime = utc_datetime.replace(tzinfo=None) # remove the timezone info for consistency
        if (model_mode == ""):
-             model_mode = "forecast"
+             model_mode = 'forecast'
 else:
        utc_datetime = datetime.strptime(run_time, '%Y%m%d_%H%M')
        if (model_mode == ""):
-             model_mode = "historical"
+             model_mode = 'historical'
 
 utc_time = utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
 utc_time_json = utc_datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -66,9 +66,7 @@ startdate = (utc_datetime - timedelta(days=7) ).strftime("%Y-%m-%d")
 
 #------ get the list for target date -----
 url_flare = "https://kauai.ccmc.gsfc.nasa.gov/DONKI/WS/get/FLR?startDate="+startdate+"&endDate="+enddate
-print('DONKI URL:', file=sys.stderr)
-print(url_flare, file=sys.stderr)
-
+print('DONKI URL:', url_flare, file=sys.stderr)
 
 # constants to avoid infinite loops during DONKI requests
 MAX_REQUESTS = 100
@@ -113,7 +111,9 @@ with open(root_dir+'/Flare/past.json') as flare_list:
 
 for i in range(0, len(data)):
    try:
-      flare_start_time = data[i].get('flrID').split("-FLR-")[0]
+      # expected ID format: yyyy-mm-ddTHH:MM:SS-CME-xxx
+      flare_id = data[i].get('flrID')
+      flare_start_time = flare_id.split("-FLR-")[0]
 
       datetime_flare = datetime.strptime(flare_start_time, '%Y-%m-%dT%H:%M:%S')
       print('[{:2d}] Flare date: {}'.format(i, datetime_flare), file=sys.stderr)
@@ -125,12 +125,12 @@ for i in range(0, len(data)):
 
       if datetime_flare > dt_start and datetime_flare <= utc_datetime:
          # check whether this flare has been simulated before
-         result = [x for x in list_obj if x.get('flrID')==data[i].get('flrID')]
+         result = [x for x in list_obj if x.get('flrID')==flare_id]
 
          if result == []:
             # no run found for this flare, new flare detected:
             flare_index.append(i)
-            print('     New flare found:', data[i].get('flrID'), file=sys.stderr)
+            print('     New flare found:', flare_id, file=sys.stderr)
          else:
             print('     Previous simulation run found:', result, file=sys.stderr)
 
@@ -146,22 +146,67 @@ f4 = open(root_dir+'/Flare/log.txt', 'a')
 
 #### check if there is a background solar wind setup in the most recent 8-hour window
 # now assuming there can be only at most 1 flare in the 15 mins time window
-print('Last flare_index', ii, file=sys.stderr)
+print('Last flare_index:', ii, file=sys.stderr)
 
 if len(flare_index) != 0:
-    list_obj.append({"flrID":data[flare_index[ii]].get('flrID')})
+    try:
+       flare_id         = data[flare_index[ii]].get('flrID')
+       flare_link       = data[flare_index[ii]].get('link')
+       flare_class      = data[flare_index[ii]].get('classType')
+       flare_location   = data[flare_index[ii]].get('sourceLocation')
+       flare_begin_time = data[flare_index[ii]].get('beginTime')
+       flare_peak_time  = data[flare_index[ii]].get('peakTime')
+       flare_end_time   = data[flare_index[ii]].get('endTime')
+
+       flare_start_time = flare_id.split("-FLR-")[0]
+       datetime_flare = datetime.strptime(flare_start_time, '%Y-%m-%dT%H:%M:%S')
+
+       target_date = datetime_flare.strftime('%Y-%m-%d')
+       t1 = datetime.strptime(target_date, '%Y-%m-%d')     # 00:00
+       t2 = t1 + timedelta(hours=8)                        # 08:00
+       t3 = t2 + timedelta(hours=8)                        # 16:00
+
+       # derive CME speed from flare class
+       class_tokens = list(flare_class)
+       class_num = float(class_tokens[1]+class_tokens[2]+class_tokens[3])
+       if class_tokens[0] == 'X':
+           FSXR = class_num*1e-4
+       if class_tokens[0] == 'M':
+           FSXR = class_num*1e-5
+       if class_tokens[0] == 'C':
+           FSXR = class_num*1e-6
+       if class_tokens[0] == 'B':
+           FSXR = class_num*1e-7
+
+       Vcme = 2.4e4*FSXR**0.3 # km/s
+       width = Vcme/25.+60.
+       if width >= 140.:
+           width = 140.
+
+       loc_tokens = list(flare_location)
+       angle = int(loc_tokens[4]+loc_tokens[5])
+       if loc_tokens[3]=='W':
+           phi_e = 100 - angle
+       if loc_tokens[3]=='E':
+           phi_e = 100 + angle
+
+       print('Flare class = {} -> Vcme = {}, location = {} -> phi_e = {}'.format(
+          flare_class, Vcme, flare_location, phi_e), file=sys.stderr)
+
+    except Exception as e:
+       traceback.print_exception(e)
+       print('[{:2d}] JSON data:'.format(i), file=sys.stderr)
+       json.dump(data[flare_index[ii]], sys.stderr, indent=3)
+
+    list_obj.append({
+      "flrID": flare_id
+    })
     with open(root_dir+'/Flare/past.json', 'w') as write_file:
        json.dump(list_obj, write_file, indent=4)
 
-    flare_start_time = data[flare_index[ii]].get('flrID').split("-FLR-")[0]
-    datetime_flare = datetime.strptime(flare_start_time, '%Y-%m-%dT%H:%M:%S')
-
-    target_date = datetime_flare.strftime('%Y-%m-%d')
-    t1 = datetime.strptime(target_date, '%Y-%m-%d')     # 00:00
-    t2 = t1 + timedelta(hours=8)                        # 08:00
-    t3 = t2 + timedelta(hours=8)                        # 16:00
-
-    f4.write('Checking Time:{} | flare found:{} class:{}\n'.format(utc_time, data[flare_index[ii]].get('flrID'), data[flare_index[ii]].get('classType')))
+    f4.write('Checking Time:{} | flare found:{} class:{} location:{} begin:{} peak:{} end:{}\n'.format(
+      utc_time, flare_id, flare_class, flare_location,
+      flare_begin_time, flare_peak_time, flare_end_time))
 
     # assuming the background solar wind setup can finish in 15 mins
     if datetime_flare < t2:
@@ -180,43 +225,9 @@ if len(flare_index) != 0:
        f4.close()
        exit_after_error(utc_time, 'Missing background folder', 'ERROR:MISSING_BKG')
 
-
     #### modify input.json for the flare run
-    f2 = open(root_dir+'/Background/'+bgsw_folder_name+'/input.json', 'r')
-    input_data = json.load(f2)
-    f2.close()
-
-    # derive CME speed from flare class
-    flare_class = data[flare_index[ii]].get('classType')
-    class_tokens = list(flare_class)
-    class_num = float(class_tokens[1]+class_tokens[2]+class_tokens[3])
-
-    if class_tokens[0] == 'X':
-        FSXR = class_num*1e-4
-    if class_tokens[0] == 'M':
-        FSXR = class_num*1e-5
-    if class_tokens[0] == 'C':
-        FSXR = class_num*1e-6
-    if class_tokens[0] == 'B':
-        FSXR = class_num*1e-7
-
-    Vcme = 2.4e4*FSXR**0.3 # km/s
-
-    width = Vcme/25.+60.
-    if width >= 140.:
-        width = 140.
-
-    print('Flare_class = {}, Vcme = {}'.format(flare_class, Vcme), file=sys.stderr)
-
-    location = data[flare_index[ii]].get('sourceLocation')
-    loc_tokens = list(location)
-    angle = int(loc_tokens[4]+loc_tokens[5])
-    if loc_tokens[3]=='W':
-        phi_e = 100 - angle
-    if loc_tokens[3]=='E':
-        phi_e = 100 + angle
-
-    print('Flare location = {}, phi_e = {}'.format(location, phi_e), file=sys.stderr)
+    with open(root_dir+'/Background/'+bgsw_folder_name+'/input.json', 'r') as f2:
+       input_data = json.load(f2)
 
     input_data['cme_speed'] = Vcme
     input_data['cme_width'] = int(width)
@@ -227,12 +238,10 @@ if len(flare_index) != 0:
         n_multi = 4.0
     else:
         n_multi = Vcme*2e-3 + 1.
-
     input_data['n_multi'] = n_multi
 
-    f3 = open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_flare_earth_input.json', 'w')
-    json.dump(input_data, f3, indent=4)
-    f3.close()
+    with open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_flare_earth_input.json', 'w') as f3:
+       json.dump(input_data, f3, indent=4)
 
     earth_r, earth_lat, earth_lon = find_location('planets/earth', datetime_flare, root_dir)
 
@@ -243,12 +252,11 @@ if len(flare_index) != 0:
 
        rad, lat, lon = find_location(loc, datetime_flare, root_dir)
 
-       input_data['phi_e'] = 100.0-data[CME_index[ii]].get('longitude') + lon - earth_lon
+       input_data['phi_e'] = phi_e + lon - earth_lon
        input_data['r0_e'] = rad
 
-       f3 = open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_CME_'+obs+'_input.json', 'w')
-       json.dump(input_data, f3, indent=4)
-       f3.close()
+       with open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_CME_'+obs+'_input.json', 'w') as f3:
+          json.dump(input_data, f3, indent=4)
 
     #### Generating Output JSON
     json_data={"sep_forecast_submission":{
@@ -297,26 +305,25 @@ if len(flare_index) != 0:
         ]
     }}
 
-    flare_end_time = data[flare_index[ii]].get('endTime')
     if flare_end_time is None:
-       flare_end_time = data[flare_index[ii]].get('peakTime')
+       flare_end_time = flare_peak_time
 
     flare = {
            "flare":{
            "last_data_time": flare_end_time,
-           "start_time":data[flare_index[ii]].get('beginTime'),
-           "peak_time": data[flare_index[ii]].get('peakTime'),
+           "start_time": flare_begin_time,
+           "peak_time": flare_peak_time,
            "end_time": flare_end_time,
-           "location": data[flare_index[ii]].get('sourceLocation'),
+           "location": flare_location,
            "intensity": FSXR,
-           "urls": [ data[flare_index[ii]].get('link') ]
+           "urls": [ flare_link ]
            }
     }
     cme = {
            "cme":{
+           "start_time": flare_begin_time,
            "half_width": width/2.,
            "speed": Vcme,
-           "start_time":data[flare_index[ii]].get('beginTime'),
            }
     }
 
@@ -325,12 +332,13 @@ if len(flare_index) != 0:
 
     with open(root_dir+'/Background/'+bgsw_folder_name+'/'+run_time+'_flare_earth_output.json', 'w') as write_file:
         json.dump(json_data, write_file, indent=4)
-    flare_id = data[flare_index[ii]].get('flrID').replace('-', '', 2).replace(':', '', 2)
+    flare_id = flare_id.replace('-', '', 2).replace(':', '', 2)
 else:
     bgsw_folder_name=''
     flare_id = ''
     if len(data) > 0:
-        f4.write('Checking Time:{} | No new flare found, last flare in 7 days: {}\n'.format(utc_time, data[len(data)-1].get('flrID') ))
+        f4.write('Checking Time:{} | No new flare found, last flare in 7 days: {}\n'.format(
+            utc_time, data[len(data)-1].get('flrID') ))
     else:
         f4.write('Checking Time:{} | No new flare found, no flare in past 7 days.\n'.format(utc_time))
 f4.close()
