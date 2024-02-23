20b6d5bb854a03b1edc79bb786119dbba5599236
iPATH User <m_ipath@ip-172-30-54-224.us-west-2.compute.internal>
2024-02-23 16:39:19 +0000

    Compress archived output files, grouping them by event start date

--- DailyStagingToArchive.sh
+++ DailyStagingToArchive.sh
@@ -13,3 +13,34 @@ find $StagingDir -type f -mtime +$nDays \
    cp -p $path ${path/staging/archive} && rm $path
 done
 
+echo
+echo "Creating iSWA tar.gz archives:"
+cd $StagingDir/iswa
+ls | grep -v tar.gz | cut -d'_' -f1-4 | sort -V | uniq -c \
+| while read n f; do
+   # if there's already an archive with the same prefix from previous runs,
+   # first extract files from it, so all files are added to the same archive
+   [[ -f $f.tar.gz ]] && {
+      tar -zxf $f.tar.gz
+      rm $f.tar.gz
+      n="$n UPDATE"
+   }
+   echo $f $n
+   tar --remove-files -zcf $f.tar.gz ${f}_*
+done
+
+echo
+echo "Creating SEP Scoreboard tar.gz archives:"
+cd $StagingDir/sep_scoreboad
+ls | grep -v tar.gz | cut -d'.' -f1-2 | sort -V | uniq -c \
+| while read n f; do
+   # if there's already an archive with the same prefix from previous runs,
+   # first extract files from it, so all files are added to the same archive
+   [[ -f $f.tar.gz ]] && {
+      tar -zxf $f.tar.gz
+      rm $f.tar.gz
+      n="$n UPDATE"
+   }
+   echo $f $n
+   tar --remove-files -zcf $f.tar.gz ${f}.*-*
+done
